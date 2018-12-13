3aa1da30022b74bf0ffa0f8c9eec098ff66ffc05
Chang Sophia <you@example.com>
2018-12-14 07:53:12 +0800

    Initial Commit

---
+++ aquaman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aquaman
+//
+//  Created by Chang Sophia on 12/14/18.
+//  Copyright © 2018 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
